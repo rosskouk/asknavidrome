d8409b1fcade3e6c44a2d99db5644f418550ed31
Ross Stewart <rosskouk@users.noreply.github.com>
2025-08-29 16:06:22 +0000

    Documentation update

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,13 +1,12 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
+    SOURCELINK_SUFFIX: '',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
