da6e0ff3170f31ec482d691db2e9b33334b2a3d0
Ross Stewart <rosskouk@gmail.com>
2025-09-08 16:43:52 +0100

    Bump docs version number

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -22,7 +22,7 @@ copyright = '2025, Ross Stewart'
 author = 'Ross Stewart'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8'
+release = '0.9'
 
 
 # -- General configuration ---------------------------------------------------
